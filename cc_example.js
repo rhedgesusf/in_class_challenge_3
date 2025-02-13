d87d3941111a27c2afcaba8c64ff183877b88cc7
Riley Hedges <rhedges1@usf.edu>
2025-02-13 15:50:01 -0500

    Task 5

--- cc_example.js
+++ cc_example.js
@@ -1,2 +1,14 @@
 // Task 5
 
+let orders = [
+    {id: 101, customer: "Alice", total: 300},
+    {id: 102, customer: "Bob", total: 450},
+    {id: 103, customer: "Charlie", total: 200}
+];
+
+function findOrder(orders, orderId) {
+    return orders.find(order => orderId === orderId);
+}
+
+console.log(findOrder(orders,102));
+
